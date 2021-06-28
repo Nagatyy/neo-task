c1e744ab5f1c270b9bb3d20f180a28ae2dcc34b9
nagatyy <mohamednagaty@aucegypt.edu>
2021-06-28 18:40:16 +0400

    Initial project skeleton

---
+++ src/main/java/com/nagaty/neotask/NeoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nagaty.neotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nagaty/neotask/NeoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagaty.neotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
